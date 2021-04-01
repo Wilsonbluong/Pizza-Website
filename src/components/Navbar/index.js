b0652a73bdf2ee97da77bc7128dacc512bc9be92
Wilson Luong <wilsonbluong@gmail.com>
2021-03-31 18:57:06 -0700

    first commit

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Nav, NavLink, NavIcon, Bars } from "./NavbarElements";
+
+const Navbar = ({ toggle, isOpen }) => {
+  return (
+    <>
+      <Nav>
+        <NavLink to="/">Pizza Factory</NavLink>
+        <NavIcon onClick={toggle}>
+          <p>Menu</p>
+          <Bars />
+        </NavIcon>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
